21cdc193bfd813b61816c7a168bf992c5468f34d
patata85 <oscarrico333@gmail.com>
2024-06-22 20:43:04 +0200

    first commit

---
+++ enderPut.lua
@@ -0,0 +1,6 @@
+local echest = peripheral.wrap("top")
+
+while true do
+    turtle.suck(64)
+    turtle.dropUp(64)
+end
---
+++ updateFiles.lua
@@ -0,0 +1,5 @@
+local files = fs.list("/rom/")
+for i = 1, #files do
+    print(files[i])
+    
+end
