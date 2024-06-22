a741b893dac79853b5cb4cde9299d217e4ac2a73
patata85 <oscarrico333@gmail.com>
2024-06-22 20:57:41 +0200

    a

--- updateFiles.lua
+++ updateFiles.lua
@@ -1,5 +1,9 @@
 local files = fs.list("/rom/")
 for i = 1, #files do
     print(files[i])
-    
+    content = http.get("https://raw.githubusercontent.com/OscarSalu85/CC-Mason/main/" .. files[i] .. ".lua").readAll()
+    if content then
+        f = fs.open(files[i], "w")
+        f.write(content)
+    end    
 end
