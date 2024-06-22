4995eb59ed609df1009ba27f661ed56daab1b6d7
patata85 <oscarrico333@gmail.com>
2024-06-22 21:56:29 +0200

    r

--- getAllFiles.lua
+++ getAllFiles.lua
@@ -3,8 +3,9 @@ if content then
     r = textutils.unserialiseJSON(content)
     for i = 1, #r.tree do
         name = r.tree[i].path
+        name = string.sub(name, (#text - 4))
         print(name)
-        url = "https://raw.githubusercontent.com/OscarSalu85/CC-Mason/main/" .. name
+        url = "https://raw.githubusercontent.com/OscarSalu85/CC-Mason/main/" .. name .. ".lua"
         file = http.get(url).readAll()
         if file then
             f = fs.open(name,"w")
