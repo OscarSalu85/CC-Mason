f2ef37ce05009a0f0430b691a40af2074a4e5e40
patata85 <oscarrico333@gmail.com>
2024-06-23 18:07:03 +0200

    bucle to wapo

--- animation.lua
+++ animation.lua
@@ -8,16 +8,18 @@ body = json.animations.animation_template.bones.body.rotation
 rightarm = json.animations.animation_template.bones.rightarm.rotation
 leftarm = json.animations.animation_template.bones.leftarm.rotation
 
-for i=1, #head do
-    Animatronic.setBodyRot(body[i][1],body[i][2],body[i][3])
-    Animatronic.setHeadRot(head[i][1],head[i][2],head[i][3])
-    Animatronic.setLeftArmRot(leftarm[i][1],leftarm[i][2],leftarm[i][3])
-    Animatronic.setRightArmRot(rightarm[i][1],rightarm[i][2],rightarm[i][3])
+while true do
+    for i=1, #head do
+        Animatronic.setBodyRot(body[i][1],body[i][2],body[i][3])
+        Animatronic.setHeadRot(head[i][1],head[i][2],head[i][3])
+        Animatronic.setLeftArmRot(leftarm[i][1],leftarm[i][2],leftarm[i][3])
+        Animatronic.setRightArmRot(rightarm[i][1],rightarm[i][2],rightarm[i][3])
+        Animatronic.push()
+    end
+    
+    Animatronic.setBodyRot(0,0,0)
+    Animatronic.setHeadRot(0,0,0)
+    Animatronic.setLeftArmRot(0,0,0)
+    Animatronic.setRightArmRot(0,0,0)
     Animatronic.push()
 end
-
-Animatronic.setBodyRot(0,0,0)
-Animatronic.setHeadRot(0,0,0)
-Animatronic.setLeftArmRot(0,0,0)
-Animatronic.setRightArmRot(0,0,0)
-Animatronic.push()
