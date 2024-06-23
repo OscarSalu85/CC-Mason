be52d1303a6de7b8c78ee2314b758161ea4d7ff0
patata85 <oscarrico333@gmail.com>
2024-06-23 15:45:03 +0200

    nuke 1

--- nuke.lua
+++ nuke.lua
@@ -1,4 +1,7 @@
-turtle.forward()
-turtle.placeDown()
-redstone.setOutput("bottom", true)
-turtle.forward()
+args = {...}
+for i =1 ,args[1] do
+    turtle.forward()
+    turtle.placeDown()
+    redstone.setOutput("bottom", true)
+    turtle.forward()
+end
