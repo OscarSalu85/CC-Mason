848ea7b9920e8d8e381e5e79b12076f448ab49fb
patata85 <oscarrico333@gmail.com>
2024-06-23 17:57:57 +0200

    :)

--- animation.lua
+++ animation.lua
@@ -1,11 +1,17 @@
+Animatronic = peripheral.wrap("top")
+
 content = fs.open("saludoRoig.json","r").readAll()
 json = textutils.unserialiseJSON(content)
 
-head = json.animations.animation_template.bones.head
-body = json.animations.animation_template.bones.body
-rightarm = json.animations.animation_template.bones.rightarm
-leftarm = json.animations.animation_template.bones.leftarm
+head = json.animations.animation_template.bones.head.rotation
+body = json.animations.animation_template.bones.body.rotation
+rightarm = json.animations.animation_template.bones.rightarm.rotation
+leftarm = json.animations.animation_template.bones.leftarm.rotation
 
-for k ,v in pairs(head) do
-    print(v[1][1])
+for i=1, #head do
+    Animatronic.setBodyRot(body[i][1],body[i][2],body[i][3])
+    Animatronic.setHeadRot(head[i][1],head[i][2],head[i][3])
+    Animatronic.setLeftArmRot(leftarm[i][1],leftarm[i][2],leftarm[i][3])
+    Animatronic.setRightArmRot(rightarm[i][1],rightarm[i][2],rightarm[i][3])
+    Animatronic.push()
 end
