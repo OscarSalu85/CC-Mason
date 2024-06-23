b9d430c570ded4ac779b5617ef5d8321641142c5
patata85 <oscarrico333@gmail.com>
2024-06-23 17:37:42 +0200

    fix

--- animation.lua
+++ animation.lua
@@ -1,9 +1,9 @@
 content = f.open("saludoRoig.json","r").readAll()
 json = textutils.unserialiseJSON(content)
 
-head = json.animations.animations.template.bones.head
-body = json.animations.animations.template.bones.body
-rightarm = json.animations.animations.template.bones.rightarm
-leftarm = json.animations.animations.template.bones.leftarm
+head = json.animations.animations_template.bones.head
+body = json.animations.animations_template.bones.body
+rightarm = json.animations.animations_template.bones.rightarm
+leftarm = json.animations.animations_template.bones.leftarm
 
 print(head.rotation[1][1])
