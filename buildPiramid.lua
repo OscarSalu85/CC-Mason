083f3e6fd45c96c2109d178ed58d2e93f698ac17
patata85 <oscarrico333@gmail.com>
2024-06-23 14:04:47 +0200

    a

--- buildPiramid.lua
+++ buildPiramid.lua
@@ -1,2 +1,27 @@
 io.write("Input lenght of the base:")
 base = io.read()
+
+height = math.ceil(base/2)
+turtle.up()
+turtle.forward()
+for i = 1, height do
+    newBase = base / 2^(i-1)
+    for x = 1, newBase do
+        for z = 1, newBase do
+            turtle.placeDown()
+            turtle.forward()
+        end
+        if x % 2 then
+            turtle.turnLeft()
+            turtle.forward()
+            turtle.turnLeft()
+        else
+            turtle.turnRight()
+            turtle.forward()
+            turtle.turnRight()
+        end
+    end
+    turtle.turnLeft()
+    turtle.turnLeft()
+    turtle.up()
+end
