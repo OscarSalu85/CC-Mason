0c88c189fee6b53e1bff46b9fc96e0f15dd369f7
patata85 <oscarrico333@gmail.com>
2024-06-28 18:08:57 +0200

    trampas

---
+++ trampas.lua
@@ -0,0 +1,10 @@
+reader = peripheral.wrap("front")
+writer = peripheral.find("nbtStorage")
+
+state = reader.getBlockData()
+io.write(state.mana)
+state.mana = 5000
+storage.writeTable(state)
+
+state2 = reader.getBlockData()
+io.write(textutils.serialise(state2))
