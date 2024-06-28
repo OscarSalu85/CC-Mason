5707226b3ffc1c5fa9dc614022bf2d94b9fe7bc2
patata85 <oscarrico333@gmail.com>
2024-06-28 18:04:11 +0200

    puede

--- nbt.lua
+++ nbt.lua
@@ -2,4 +2,6 @@ reader = peripheral.wrap("bottom")
 
 state = reader.getBlockData()
 io.write(state.mana)
+--io.write(textutils.serialise(state))
+state.mana = 5000
 io.write(textutils.serialise(state))
