39ff47fdb0975b85e42b47db43b31a44d56f5b20
patata85 <oscarrico333@gmail.com>
2024-06-28 13:41:50 +0200

    aaaaah

--- roigLaden.lua
+++ roigLaden.lua
@@ -1,4 +1,15 @@
 monitor = peripheral.wrap("left")
 
 suc, data = turtle.inspectDown()
-monitor.write(textutils.serialise(data))
+active = true
+pointer = 1
+while active do
+    text = textutils.serialise(data):sub(pointer, pointer + 25)
+    if text == nil then
+        active = false
+    else
+        monitor.write(text)
+        pointer = pointer + 25
+    end
+    
+end
