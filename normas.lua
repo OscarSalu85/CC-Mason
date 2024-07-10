849569f96a8cb861c726fbcc0b4a040de8c940e8
patata85 <oscarrico333@gmail.com>
2024-07-10 19:16:43 +0200

    bbbbbbbbbb

--- normas.lua
+++ normas.lua
@@ -2,7 +2,7 @@ chat = peripheral.find("chatBox")
 while true do
     local event, username, message = os.pullEvent("chat")
     inicio = string.sub(message,1,6)
-    final = string.sub(message,8)
+    final = string.sub(message,7)
     if inicio == "permit" then
         if final == " help" then
             chat.sendMessageToPlayer("Toma ayuda bobo",username, "Server")
