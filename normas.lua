e3e7da372f13567783979b1017d0eafe258c1764
patata85 <oscarrico333@gmail.com>
2024-07-10 19:18:28 +0200

    holaa

--- normas.lua
+++ normas.lua
@@ -7,6 +7,7 @@ while true do
         if final == " help" then
             chat.sendMessageToPlayer("Toma ayuda bobo",username, "Server")
         elseif final == " bobo" then
+            io.write("Bobo --> " .. username)
             commands.exec("team add " .. username)
             commands.exec("team modify " .. username .." prefix [Bobo]")
             commands.exec("team join " .. username .. " " .. username)
