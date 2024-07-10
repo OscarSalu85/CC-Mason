cf806311c8e4623a9d56d78bede90d4fec942920
patata85 <oscarrico333@gmail.com>
2024-07-10 23:22:44 +0200

    preuba

--- normas.lua
+++ normas.lua
@@ -7,7 +7,13 @@ while true do
         if command == "help" then
             chat.sendMessageToPlayer("Comandos disponibles:",username, "Server")
             sleep(1)
-            chat.sendMessageToPlayer("- 'permit help' -> Ver todos los comandos disponibles",username, "Server")
+            phelp = {
+                {text = "- "},
+                {text = "'permit help'" , color = "red"},
+                {text = " -> Ver todos los comandos disponibles" , color = "orange"}
+            }
+            --chat.sendMessageToPlayer("- 'permit help' -> Ver todos los comandos disponibles",username, "Server")
+            chat.sendFormattedMessageToPlayer(phelp,username, "Server")
             sleep(1)
             chat.sendMessageToPlayer("- 'permit edit <Modificar Casa> <Matar> <Robar>' -> Editar permisos de cara a otros jugadores, rellenar con S/N (Si o No)",username, "Server")
         elseif command == "edit" then
