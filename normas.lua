41d77024110e1caa5a6dc93426c83b4487cac41b
patata85 <oscarrico333@gmail.com>
2024-07-10 18:38:26 +0200

    ayufa

--- normas.lua
+++ normas.lua
@@ -3,7 +3,7 @@ while true do
     local event, username, message = os.pullEvent("chat")
     inicio = string.sub(message,1,7)
     final = string.sub(message,7)
-    if inicio == "$permit" then
+    if inicio == "~permit" then
         if final == " help" then
             chat.sendMessage("Toma ayuda bobo", "El computer")
         else
