8c4eeadd8d45449d16009aef9cdf20fca99e3bde
patata85 <oscarrico333@gmail.com>
2024-07-10 23:09:38 +0200

    ups2

--- updateFiles.lua
+++ updateFiles.lua
@@ -3,10 +3,12 @@ local files = fs.list("")
 for i = 1, #files do
     if files[i] ~= "rom" then
         print(files[i])
-        content = http.get("https://raw.githubusercontent.com/OscarSalu85/CC-Mason/main/" .. files[i] .. ".lua").readAll()
-        if content then
-            f = fs.open(files[i], "w")
-            f.write(content)
+        if files[i] ~= "startup.lua" then
+            content = http.get("https://raw.githubusercontent.com/OscarSalu85/CC-Mason/main/" .. files[i] .. ".lua").readAll()
+            if content then
+                f = fs.open(files[i], "w")
+                f.write(content)
+            end
         end
     end    
 end
