31bbefde1cdb8351f017a5dd2576b29c924a1a5c
patata85 <oscarrico333@gmail.com>
2024-07-10 19:06:30 +0200

    ayuda

--- normas.lua
+++ normas.lua
@@ -3,7 +3,7 @@ while true do
     local event, username, message = os.pullEvent("chat")
     inicio = string.sub(message,1,6)
     final = string.sub(message,8)
-    if inicio == "~permit" then
+    if inicio == "permit" then
         if final == " help" then
             chat.sendMessageToPlayer("Toma ayuda bobo",username, "Server")
         elseif final == "bobo" then
