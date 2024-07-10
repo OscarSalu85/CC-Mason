ab2d4e1acf28c298e3f65d6e427cb859d5deb1a5
patata85 <oscarrico333@gmail.com>
2024-07-10 23:00:41 +0200

    fix 2

--- normas.lua
+++ normas.lua
@@ -6,7 +6,7 @@ while true do
     if inicio == "permit" then
         --io.write(command)
         if command == "help" then
-            chat.sendMessageToPlayer("Comandos disponibles:",username, "Server", "Box", "<>", "&b")
+            chat.sendMessageToPlayer("Comandos disponibles:",username, "Server")
             sleep(1)
             chat.sendMessageToPlayer("- 'permit help' -> Ver todos los comandos disponibles",username, "Server")
             sleep(1)
