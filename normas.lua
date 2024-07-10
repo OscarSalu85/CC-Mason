8074ba79aa7bef50e214fac127531af3420ea67b
patata85 <oscarrico333@gmail.com>
2024-07-10 19:34:37 +0200

    Prueba

--- normas.lua
+++ normas.lua
@@ -9,7 +9,7 @@ while true do
         elseif final == " bobo" then
             io.write("Bobo --> " .. username)
             commands.exec("team add " .. username)
-            commands.exec("team modify " .. username ..' prefix "[Bobo]"')
+            commands.exec("team modify " .. username ..' prefix {"text":"[","color":"red","extra":[{"text":"BOBO","color":"white"},{"text":"]","color":"red"}]}')
             commands.exec("team join " .. username .. " " .. username)
 
         else
