057f4c65a580a5eefb9a942771dce837ab5a663d
patata85 <oscarrico333@gmail.com>
2024-07-11 12:35:06 +0200

    puede

--- updateFiles.lua
+++ updateFiles.lua
@@ -3,9 +3,10 @@ local files = fs.list("")
 for i = 1, #files do
     if files[i] ~= "rom" then
         print(files[i])
-        if files[i] ~= "startup.lua" or string.sub(files[i],-5,-1) ~= ".json" then
-            content = http.get("https://raw.githubusercontent.com/OscarSalu85/CC-Mason/main/" .. files[i] .. ".lua").readAll()
+        if files[i] ~= "startup.lua" then
+            content = http.get("https://raw.githubusercontent.com/OscarSalu85/CC-Mason/main/" .. files[i] .. ".lua")
             if content then
+                content = content.readAll()
                 f = fs.open(files[i], "w")
                 f.write(content)
             end
