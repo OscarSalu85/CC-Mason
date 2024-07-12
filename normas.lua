96951f75e01ece835ac1da58149508587a76c2d6
patata85 <oscarrico333@gmail.com>
2024-07-12 14:20:33 +0200

    aaaaaaaaaaaa

--- normas.lua
+++ normas.lua
@@ -109,7 +109,7 @@ while true do
                 content = fs.open("permit.json","r").readAll()
                 json = textutils.unserialiseJSON(content)
                 --json[username] = "MC-" .. modif .. " M-" .. matar .. " R-" .. robar
-                json[username] = {{text = "MC ", color = modif},{text = "M ", color = matar},{text = "R", color = robar}}
+                json[username].insert({text ="MC ", color = modif},1)
                 json = textutils.serialiseJSON(json)
                 chat.sendFormattedMessageToPlayer(json,username, "Server")
                 --file = fs.open("permit.json","w")
