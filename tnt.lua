e096d901819594abcb65b8f3233ecb13dbf20c0d
patata85 <oscarrico333@gmail.com>
2024-07-17 22:55:20 +0200

    aaaaaaaa

--- tnt.lua
+++ tnt.lua
@@ -1,5 +1,6 @@
 pchest = peripheral.wrap("front")
 achest= peripheral.wrap("right")
+rchest= peripheral.wrap("down")
 
 function checkChest(c)
     count = 0
@@ -16,34 +17,36 @@ function checkChest(c)
 end
 
 while true do
-    turtle.select(1)
-    turtle.suck(10)
+    if checkChest(rchest) then
+        turtle.select(1)
+        turtle.suck(10)
 
-    turtle.select(2)
-    turtle.suckUp(10)
+        turtle.select(2)
+        turtle.suckUp(10)
 
-    turtle.select(3)
-    turtle.suck(10)
+        turtle.select(3)
+        turtle.suck(10)
 
-    turtle.select(5)
-    turtle.suckUp(10)
+        turtle.select(5)
+        turtle.suckUp(10)
 
-    turtle.select(6)
-    turtle.suck(10)
+        turtle.select(6)
+        turtle.suck(10)
 
-    turtle.select(7)
-    turtle.suckUp(10)
+        turtle.select(7)
+        turtle.suckUp(10)
 
-    turtle.select(9)
-    turtle.suck(10)
+        turtle.select(9)
+        turtle.suck(10)
 
-    turtle.select(10)
-    turtle.suckUp(10)
+        turtle.select(10)
+        turtle.suckUp(10)
 
-    turtle.select(11)
-    turtle.suck(10)
+        turtle.select(11)
+        turtle.suck(10)
 
-    turtle.select(16)
-    turtle.craft(10)
-    turtle.dropDown(10)
+        turtle.select(16)
+        turtle.craft(10)
+        turtle.dropDown(10)
+    end
 end
