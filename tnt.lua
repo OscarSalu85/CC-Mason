5cf0986889b78a9998ccfc8ade5b1ce35da7cbd8
patata85 <oscarrico333@gmail.com>
2024-07-17 22:53:33 +0200

    aaaaa

--- tnt.lua
+++ tnt.lua
@@ -3,32 +3,33 @@ achest= peripheral.wrap("right")
 
 while true do
     turtle.select(1)
-    turtle.suck(1)
+    turtle.suck(10)
 
     turtle.select(2)
-    turtle.suckUp(1)
+    turtle.suckUp(10)
 
     turtle.select(3)
-    turtle.suck(1)
+    turtle.suck(10)
 
     turtle.select(5)
-    turtle.suckUp(1)
+    turtle.suckUp(10)
 
     turtle.select(6)
-    turtle.suck(1)
+    turtle.suck(10)
 
     turtle.select(7)
-    turtle.suckUp(1)
+    turtle.suckUp(10)
 
     turtle.select(9)
-    turtle.suck(1)
+    turtle.suck(10)
 
     turtle.select(10)
-    turtle.suckUp(1)
+    turtle.suckUp(10)
 
     turtle.select(11)
-    turtle.suck(1)
+    turtle.suck(10)
 
     turtle.select(16)
-    turtle.craft()
+    turtle.craft(10)
+    turtle.dropDown(10)
 end
